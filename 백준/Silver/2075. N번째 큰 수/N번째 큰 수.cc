cad87910c72b3c6a9ded2b48d10abc91ead1af1c
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-08-16 23:35:16 +0900

    [Silver II] Title: N번째 큰 수, Time: 320 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -5,13 +5,13 @@ int main() {
     cin.tie(0);
     unsigned short int N;
     cin >> N;
-    priority_queue<int, vector<int>, greater<int>> pq; // Min-heap
+    priority_queue<int, vector<int>, greater<int>> pq;
     int input;
     for (int i = 0; i < N * N; i++) {
         cin >> input;
         pq.push(input);
-        if (pq.size() > N) pq.pop(); // Keep only the largest N elements
+        if (pq.size() > N) pq.pop();
     }
-    cout << pq.top(); // The N-th largest element
+    cout << pq.top();
     return 0;
 }
