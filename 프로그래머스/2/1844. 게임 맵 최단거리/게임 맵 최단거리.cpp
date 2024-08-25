2ae0158273c0b211f686a81ba3b41e5d0dfaf4b8
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-08-25 19:58:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.17 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,32 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(vector<vector<int> > maps)
+int solution(vector<vector<int>> maps)
 {
     int n = maps.size();
     int m = maps[0].size();
-    vector<vector<int>> visited(n, vector<int>(m, -1));
-    visited[0][0] = 1;
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
-    
+    int dx[4] = { -1, 1, 0, 0 };
+    int dy[4] = { 0, 0, -1, 1 };
+
     queue<pair<int, int>> bfs;
-    bfs.push({0, 0});
-    
-    while(!bfs.empty()){
+    bfs.push({ 0, 0 });
+    maps[0][0] = 3;
+
+    while (!bfs.empty()) {
         int y = bfs.front().first;
         int x = bfs.front().second;
         bfs.pop();
-        for(int i = 0 ; i < 4; i++){
+        if (y == n - 1 && x == m - 1)break;
+        for (int i = 0; i < 4; i++) {
             int col = dy[i] + y;
             int row = dx[i] + x;
-            if(col == -1 || row == -1 || col == n || row == m)continue;
-            if(maps[col][row] == 0)continue;
-            if(visited[col][row] > 0)continue;
-            visited[col][row] = visited[y][x] + 1;
-            bfs.push({col, row});
+            if (col == -1 || row == -1 || col == n || row == m)continue;
+            if (maps[col][row] != 1)continue;
+            maps[col][row] = maps[y][x] + 1;
+            bfs.push({ col, row });
         }
     }
-    
-    return visited[n-1][m-1];
+    return maps[n - 1][m - 1] - 2;
 }
