e4a09c644438e38814303b3f0859738d595b088c
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-08-31 19:34:22 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 200 ms, Memory: 22128 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -2,45 +2,28 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL);
 
-    int N;
-    cin >> N;
-    vector<int> v;
-    vector<int> idx(N), pre(N, -1);
-    vector<int> seq;
-    vector<int> save;
-    for (int i = 0; i < N; ++i) {
-        int x;
-        cin >> x;
-        save.push_back(x);
-        auto it = lower_bound(v.begin(), v.end(), x);
-        int pos = it - v.begin();
+    int n; cin >> n;
+    vector<int> lis;
+    vector<int> idx(n), pre(n, -1);
+    vector<int> v(n);
+    stack<int> s;
 
-        if (it == v.end()) {
-            v.push_back(x);
-        } else {
-            *it = x;
-        }
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+        auto it = lower_bound(lis.begin(), lis.end(), v[i]);
+        int pos = it - lis.begin();
+
+        if (it == lis.end()) lis.push_back(v[i]);
+        else *it = v[i];
 
         idx[pos] = i;
-        if (pos > 0) {
-            pre[i] = idx[pos - 1];
-        }
+        if (pos > 0) pre[i] = idx[pos - 1];
     }
 
-    cout << v.size() << '\n';
-
-    int t = idx[v.size() - 1];
-    while (t != -1) {
-        seq.push_back(t);
-        t = pre[t];
-    }
-
-    reverse(seq.begin(), seq.end());
-    for (int i : seq) {
-        cout << save[i] << ' ';
-    }
+    cout << lis.size() << '\n';
+    int t = idx[lis.size() - 1];
+    while (t != -1) {s.push(t); t = pre[t]; }
+    while (!s.empty()) { cout << v[s.top()] << ' '; s.pop(); }
 }
