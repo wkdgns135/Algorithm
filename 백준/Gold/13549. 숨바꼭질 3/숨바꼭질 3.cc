138f3ae794031e4703f43b041ed29f32d69b7832
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-08-31 16:04:29 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 3316 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define MAX_SIZE 100001
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int n, k; cin >> n >> k;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     
-    vector<pair<int, bool>> mdt(200000, { INT_MAX, false });
+    vector<pair<int, bool>> mdt(MAX_SIZE, { INT_MAX, false });
     mdt[n].first = 0;
     pq.push({ 0, n });
 
@@ -22,11 +22,11 @@ int main() {
             mdt[index - 1].first = cost + 1;
             pq.push({ cost + 1 , index - 1 });
         }
-        if (index + 1 < k * 2 && mdt[index + 1].first > cost + 1) {
+        if (index + 1 < MAX_SIZE && mdt[index + 1].first > cost + 1) {
             mdt[index + 1].first = cost + 1;
             pq.push({ cost + 1 , index + 1 });
         }
-        if (index * 2 < k * 2 && mdt[index * 2].first > cost) {
+        if (index * 2 < MAX_SIZE && mdt[index * 2].first > cost) {
             mdt[index * 2].first = cost;
             pq.push({ cost, index * 2});
         }
