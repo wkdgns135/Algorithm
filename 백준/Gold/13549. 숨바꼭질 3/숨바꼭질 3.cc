672da13623b6197ba3883fa947d8d147bb8cd4de
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-08-31 16:02:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 4096 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, k; cin >> n >> k;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    
+    vector<pair<int, bool>> mdt(200000, { INT_MAX, false });
+    mdt[n].first = 0;
+    pq.push({ 0, n });
+
+    while (!pq.empty()) {
+        int cost = pq.top().first;
+        int index = pq.top().second;
+        pq.pop();
+        if (mdt[index].second)continue;
+        if (index == k)break;
+        mdt[index].second = true;
+
+        if (index - 1 >= 0 && mdt[index - 1].first > cost + 1) {
+            mdt[index - 1].first = cost + 1;
+            pq.push({ cost + 1 , index - 1 });
+        }
+        if (index + 1 < k * 2 && mdt[index + 1].first > cost + 1) {
+            mdt[index + 1].first = cost + 1;
+            pq.push({ cost + 1 , index + 1 });
+        }
+        if (index * 2 < k * 2 && mdt[index * 2].first > cost) {
+            mdt[index * 2].first = cost;
+            pq.push({ cost, index * 2});
+        }
+    }
+
+    cout << mdt[k].first;
+}
