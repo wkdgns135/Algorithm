397d8032b0fab923c61a3f3de2ceae4563a288db
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-09-02 19:34:18 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1184 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -12,30 +12,13 @@ int main() {
         while (k--) {
             char c; cin >> c;
             int n; cin >> n;
-            if (c == 'I') {
-                ms.insert(n);  // 삽입 작업은 O(log k)
-                }
-                else {
-                    if (!ms.empty()) {
-                        if (n == 1) {
-                            auto it = ms.end();
-                            --it;  // 가장 큰 요소에 대한 iterator
-                            ms.erase(it);  // 가장 큰 요소 제거, O(log k)
-                        }
-                        else {
-                            ms.erase(ms.begin());  // 가장 작은 요소 제거, O(log k)
-                        }
-                    }
-                }
+            if (c == 'I') ms.insert(n);
+            else if (!ms.empty()) {
+                if (n == 1) ms.erase(--ms.end());
+                else ms.erase(ms.begin());
             }
-            if (ms.empty()) {
-                cout << "EMPTY" << '\n';
-            }
-            else {
-                auto min_it = ms.begin();  // 가장 작은 요소
-                auto max_it = ms.end();
-                --max_it;  // 가장 큰 요소
-                cout << *max_it << ' ' << *min_it << '\n';
         }
+        if (ms.empty())  cout << "EMPTY" << '\n';
+        else  cout << *(--ms.end()) << ' ' << *(ms.begin()) << '\n';
     }
 }
