6b9e1e8a2d238fe1d64605d6afa5d70b89363ba9
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-09-03 17:18:26 +0900

    [Silver II] Title: 나무 자르기, Time: 144 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -4,19 +4,16 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
     int n, m; cin >> n >> m;
-    
-    vector<long long> v(n + 1);
-    vector<long long> vv(n + 1);
+    vector<long long> v(n + 1), vv(n + 1);
     for (int i = 0; i < n; i++)cin >> v[i];
     sort(v.begin(), v.end(), greater<int>());
 
+    int idx = 0;
     for (int i = 1; i <= n; i++) {
         vv[i] = (v[i-1] - v[i]) * i + vv[i - 1];
+        if (!idx && vv[i] >= m)idx = i;
     }
 
-    auto it = lower_bound(vv.begin(), vv.end(), m);
-    int pos = it - vv.begin(); 
-    
-    int answer = *v.begin() - ((*v.begin() - v[pos]) - (vv[pos] - m) / pos);
+    int answer = *v.begin() - ((*v.begin() - v[idx]) - (vv[idx] - m) / idx);
     cout << answer;
 }
