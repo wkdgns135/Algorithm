a79492c5a1cf2069ec8e2c835db7e21b1c82b4c0
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-09-12 17:39:40 +0900

    [Gold V] Title: 하노이 탑, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+
+void hanoi(int depth, const int &from, const int &via, const int &to) {
+    if (depth == 1) { cout << from << ' ' << to << '\n'; }
+    else {
+        hanoi(depth - 1, from, to, via);
+        cout << from << ' ' << to << '\n';
+        hanoi(depth - 1, via, from, to);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    string a = "1";
+    for (int i = 1; i < n; i++) {
+        string b = "";
+        int carry = 0;
+        while (!a.empty()) {
+            int n1 = a.back() - '0';
+            int n2 = a.back() - '0';
+            if (b.empty())n2++;
+            a.pop_back();
+            b.push_back((n1 + n2 + carry) % 10 + '0');
+            carry = (n1 + n2 + carry) >= 10;
+        }
+        if (carry)b.push_back(carry + '0');
+        reverse(b.begin(), b.end());
+        a = b;
+    }
+    cout << a << '\n';
+    const int from = 1, via = 2, to = 3;
+    if(n < 21)hanoi(n, from, via, to);
+}
