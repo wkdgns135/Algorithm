8eb0a958ac235863bf8577c8633e86c79d5293ac
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-09-13 22:57:58 +0900

    [level 3] Title: 단속카메라, Time: 3.85 ms, Memory: 3.99 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int solution(vector<vector<int>> routes) {
-    int answer = 0;
-    int range = -INT_MAX;
-    sort(routes.begin(), routes.end(), [](vector<int> a, vector<int> b){return a[0] < b[0];});
-    
-    for(int i = 0; i < routes.size(); i++){
-        if(routes[i][0] <= range)range = min(routes[i][1], range);
-        else{answer++;range = routes[i][1];}
+    int answer = 1;
+    sort(routes.begin(), routes.end(), [](vector<int> a, vector<int> b){
+        return a[1] < b[1];
+    });
+    int last = routes[0][1];
+    for(int i = 1; i < routes.size(); i++){
+        if(last < routes[i][0]){
+            last = routes[i][1];
+            answer++;
+        }
     }
-    
     return answer;
 }
