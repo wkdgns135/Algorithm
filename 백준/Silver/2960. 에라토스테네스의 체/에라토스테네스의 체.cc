6efc1bdbba37eb21a220db660bf38ccc62fdafac
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-09-15 19:07:00 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int n, k; cin >> n >> k;
     vector<bool> v(n + 1, true);
     for (int i = 2; i <= n && k; i++) {
