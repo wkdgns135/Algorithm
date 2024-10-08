54f20bff3ddda82cb81b4e9d9047c767015cb278
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-10-09 00:06:45 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -2,13 +2,12 @@
 using namespace std;
 
 int N, M;
-void BackTracking(vector<int> v, vector<bool> isUsed, int n) {
-	if (n == M) { for (int i = 0; i < M; i++)cout << v[i] << " "; cout << "\n"; return; }
-	for (int i = 0; i < N; i++) {
-		if (isUsed[i])continue;
-		isUsed[i] = true;
-		v[n] = i + 1;
-		BackTracking(v, isUsed,n + 1);
+vector<int> v;
+void BackTracking(int depth, int index) {
+	if (depth == M) { for (int i = 0; i < M; i++)cout << v[i] << " "; cout << "\n"; return; }
+	for (int i = index; i < N; i++) {
+		v[depth] = i + 1;
+		BackTracking(depth + 1, i + 1);
 	}
 }
 
@@ -17,8 +16,6 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> N >> M;
-
-	vector<int> v(M, 0);
-	vector<bool> isUsed(N);
-	BackTracking(v, isUsed, 0);
+	v = vector<int>(M, 0);
+	BackTracking(0, 0);
 }
