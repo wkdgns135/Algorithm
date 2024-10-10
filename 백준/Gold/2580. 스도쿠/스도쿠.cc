9b511e0852479dc718ef4ff46d275a31a8d5ff65
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-10-10 20:28:07 +0900

    [Gold IV] Title: 스도쿠, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -3,52 +3,50 @@ using namespace std;
 
 vector<vector<int>> v(9, vector<int>(9));
 vector<pair<int, int>> emptyNums;
-bool findAnswer = false;
 
 bool CheckNum(int n, int x, int y) {
     for (int i = 0; i < 9; i++) {
-        if (v[y][i] == n)return false;
-        if (v[i][x] == n)return false;
+        if (v[y][i] == n) return false;
+        if (v[i][x] == n) return false;
     }
     int xx = x / 3 * 3, yy = y / 3 * 3;
     for (int i = yy; i < yy + 3; i++) {
         for (int j = xx; j < xx + 3; j++) {
-            if (v[i][j] == n)return false;
+            if (v[i][j] == n) return false;
         }
     }
     return true;
 }
 
-bool CheckEmpty() {
-    for (auto& pos : emptyNums)if (!v[pos.first][pos.second])return false;
-    return true;
-}
-
-void BackTracking(int depth) {
-    if (CheckEmpty()) 
-        findAnswer = true;
-    if (findAnswer)return;
+bool BackTracking(int depth) {
+    if (depth == emptyNums.size()) return true;
 
     for (int i = 1; i <= 9; i++) {
-        if (!CheckNum(i, emptyNums[depth].second, emptyNums[depth].first)) continue;
-        v[emptyNums[depth].first][emptyNums[depth].second] = i;
-        BackTracking(depth + 1);
-        if(!findAnswer)v[emptyNums[depth].first][emptyNums[depth].second] = 0;
+        int x = emptyNums[depth].second;
+        int y = emptyNums[depth].first;
+
+        if (!CheckNum(i, x, y)) continue;
+
+        v[y][x] = i;
+        if (BackTracking(depth + 1)) return true;
+
+        v[y][x] = 0;
     }
-    
+
+    return false;
 }
 
 int main() {
-    ios::sync_with_stdio(0);cin.tie(0);
-    for (int i = 0; i < 9; i++)for (int j = 0; j < 9; j++) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    for (int i = 0; i < 9; i++) for (int j = 0; j < 9; j++) {
         cin >> v[i][j];
-        if (v[i][j] == 0)emptyNums.push_back({ i, j });
+        if (v[i][j] == 0) emptyNums.push_back({ i, j });
     }
 
     BackTracking(0);
 
     for (auto& i : v) {
-        for (auto& j : i)cout << j << ' ';
+        for (auto& j : i) cout << j << ' ';
         cout << '\n';
     }
 }
