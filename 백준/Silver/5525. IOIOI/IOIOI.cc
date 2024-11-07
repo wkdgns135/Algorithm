40dc6cea3ed677934f9ed59e311f40a07a305db1
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-11-07 22:15:47 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -5,12 +5,22 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n, m; cin >> n >> m;
     string s; cin >> s;
-    string cmp = "I";
-    int answer = 0;
-    for (int i = 0; i < n; i++)cmp += "OI";
-    for (int i = 0; i <= m - (n * 2 + 1) ; i++) {
-        string temp = s.substr(i, n * 2 + 1);
-        if (temp == cmp)answer++;
+
+    int answer = 0, count = 0;
+    for (int i = 1; i < m - 1; i++) {
+        // IOI 패턴이 계속 이어질 때
+        if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+            count++;  // IOI 패턴 길이 카운트
+            if (count == n) { // n번 반복되면 정답 증가
+                answer++;
+                count--;  // 패턴을 한 칸 앞으로 밀기 위해 감소
+            }
+            i++;  // IOI 패턴을 찾았으니 다음 I부터 탐색
+        }
+        else {
+            count = 0;  // 패턴이 끊기면 초기화
+        }
     }
+
     cout << answer;
 }
