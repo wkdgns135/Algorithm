728e34766cdf016c5388993001168131af691ab1
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-11-14 20:41:27 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1564 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n, input; cin >> n;
-    unordered_map<int, int> dict;
+    vector<int> v(10001);
     for (int i = 0; i < n; i++) {
         cin >> input;
-        dict[input]++;
+        v[input]++;
     }
     for (int i = 1; i <= 10000; i++) {
-        for (int j = 0; j < dict[i]; j++) {
+        for (int j = 0; j < v[i]; j++) {
             cout << i << '\n';
         }
     }
