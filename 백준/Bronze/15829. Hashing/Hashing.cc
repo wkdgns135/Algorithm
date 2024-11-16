9e2df511d8f7ff0df9ff74cbfd9e07e18cd3afcc
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-11-16 20:13:49 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,10 +5,14 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
     string s; cin >> s;
-    int answer = 0;
+    const int MOD = 1234567891;
+    const int BASE = 31;
+    long long answer = 0;
+    long long power = 1; // 31^i 값을 저장할 변수
+
     for (int i = 0; i < n; i++) {
-        answer += (s[i] - 'a' + 1) * pow(31, i);
-        answer %= 1234567891;
+        answer = (answer + (s[i] - 'a' + 1) * power) % MOD;
+        power = (power * BASE) % MOD; // 매번 31^i를 갱신
     }
     cout << answer;
 }
