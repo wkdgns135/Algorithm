1bc100d16ee14b00a0423565382a7331b5011d53
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2024-12-16 16:24:12 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int a, b, c; cin >> a >> b >> c;
-	vector<vector<vector<int>>> dp(101, vector<vector<int>>(101, vector<int>(101, 1)));
-	for (int i = 51; i <= 70; i++) {
-		for (int j = 51; j <= 70; j++) {
-			for (int k = 51; k <= 70; k++) {
+	vector<vector<vector<int>>> dp(21, vector<vector<int>>(21, vector<int>(21, 1)));
+	for (int i = 1; i <= 20; i++) {
+		for (int j = 1; j <= 20; j++) {
+			for (int k = 1; k <= 20; k++) {
 				if (i < j && j < k) dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
 				else dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1];
 			}
@@ -16,8 +16,8 @@ int main() {
 	while (!(a == -1 && b == -1 && c == -1)) {
 		cout<< "w(" << a << ", " << b << ", " << c << ") = ";
 		if (a <= 0 || b <= 0 || c <= 0)cout << 1 << '\n';
-		else if(a > 20 || b > 20 || c > 20)cout << dp[70][70][70] << '\n';
-		else cout << dp[a + 50][b + 50][c + 50] << '\n';
+		else if(a > 20 || b > 20 || c > 20)cout << dp[20][20][20] << '\n';
+		else cout << dp[a][b][c] << '\n';
 		cin >> a >> b >> c;
 	}
 }
