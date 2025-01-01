f6d148ebc18dbf9d4d378b91914d162754697106
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-01-02 00:21:41 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+void func(vector<long long>& a, vector<long long>& b) {
+    long long r0 = (a[0] * b[0] + a[1] * b[2]) % MOD;
+    long long r1 = (a[0] * b[1] + a[1] * b[3]) % MOD;
+    long long r2 = (a[2] * b[0] + a[3] * b[2]) % MOD;
+    long long r3 = (a[2] * b[1] + a[3] * b[3]) % MOD;
+    a[0] = r0, a[1] = r1, a[2] = r2, a[3] = r3;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    long long n; cin >> n;
+    vector<long long> a = { 1,1,1,0 };
+    vector<long long> b = { 1,0,0,1 };
+    n += n % 2 == 0;
+    while (n) {
+        if (n % 2 == 1)func(b, a);
+        n >>= 1;
+        func(a, a);
+    }
+
+    cout << (b[1] - 1) % MOD;
+}
