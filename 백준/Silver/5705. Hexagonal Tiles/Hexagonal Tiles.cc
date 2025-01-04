4187f549e276f196cb36a5d9a7b7b36e73416a27
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-01-04 13:30:31 +0900

    [Silver IV] Title: Hexagonal Tiles, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/5705. Hexagonal Tiles/Hexagonal Tiles.cc
+++ 백준/Silver/5705. Hexagonal Tiles/Hexagonal Tiles.cc
@@ -1,2 +1,2 @@
 #include<iostream>
-using namespace std; int main() { int a[41] = { 1,1 }, i; for (i = 2; i < 41; i++)a[i] = a[i - 1] + a[i - 2]; while (cin >> i && i)cout << a[i] << '\n'; }
+using namespace std;int main(){int a[41]={1,1},i;for(i=2;i<41;i++)a[i]=a[i-1]+a[i-2];while(cin>>i&&i)cout<<a[i]<<'\n';}
