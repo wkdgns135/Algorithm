e6e3d4a561ea9a6611688f09adf3cb603fb89d0f
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-02-16 23:40:07 +0900

    [Gold V] Title: 강의실 배정, Time: 48 ms, Memory: 4484 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    sort(v.begin(), v.end(), [](pair<int, int>& a, pair<int, int>& b) {
+        if (a.first != b.first)return a.first < b.first;
+        return a.second > b.second;
+        });
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(v[0].second);
+    int answer = 1;
+    for (int i = 1; i < n; i++) {
+        if (pq.top() > v[i].first) {
+            pq.push(v[i].second);
+            answer++;
+        }
+        else {
+            pq.pop();
+            pq.push(v[i].second);
+        }
+    }
+    cout << answer;
+}
