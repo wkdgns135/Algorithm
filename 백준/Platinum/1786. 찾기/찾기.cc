20d3ed2c8d620ad4e9ac0574faf26455eb7bd96b
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-03-28 19:48:59 +0900

    [Platinum V] Title: 찾기, Time: 48 ms, Memory: 9432 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> pi;
+
+void Kmp(string& pat)
+{
+    int n = pat.size();
+    pi.resize(n, 0);  // 벡터 크기를 패턴 길이와 동일하게 조정
+    int j = 0;
+    for (int i = 1; i < n; i++)
+    {
+        while (j > 0 && pat[i] != pat[j])
+        {
+            j = pi[j - 1];
+        }
+        if (pat[i] == pat[j])
+        {
+            j++;
+        }
+        pi[i] = j;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    string pat, str;
+    getline(cin, str);
+    getline(cin, pat);
+
+    // 접두사 함수 계산
+    Kmp(pat);
+
+    int n = str.size();
+    int m = pat.size();
+    int j = 0;
+    vector<int> answer;
+    for (int i = 0; i < n; i++)
+    {
+        while (j > 0 && str[i] != pat[j])
+        {
+            j = pi[j - 1];
+        }
+
+        if (str[i] == pat[j])
+        {
+            j++;
+        }
+
+        if (j == m)
+        {
+            answer.push_back(i - m + 2);
+            j = pi[j - 1];
+        }
+    }
+
+    cout << answer.size() << '\n';
+    for (const int& num : answer)
+    {
+        cout << num << ' ';
+    }
+}
