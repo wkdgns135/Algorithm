692483d4a5ae9b26d5800e958e51b5bc1662a600
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-17 15:53:35 +0900

    [level 2] Title: 큰 수 만들기, Time: 3612.32 ms, Memory: 5.34 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(string number, int k) {
+    while(k--){
+        auto iter = number.begin();
+        for(iter; iter != number.end(); iter++){
+            if(*iter < *next(iter)){
+                number.erase(iter);
+                break;
+            }
+        }
+        if(iter == number.end()) number.erase(min_element(number.begin(), number.end()));
+    }
+    return number;
+}
