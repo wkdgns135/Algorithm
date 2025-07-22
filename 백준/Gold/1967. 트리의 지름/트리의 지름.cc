72dc811450180c38c14f49dcec2e72c8ba6fa011
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-22 23:08:38 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3304 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,44 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<pair<int, int>>> graph;
+struct Node;
 
-// 가장 먼 노드와 거리를 반환하는 DFS 함수
-pair<int, int> dfs(int node, int dist, vector<bool>& visited) {
-    visited[node] = true;
-    pair<int, int> farthest = { dist, node };
+struct Edge
+{
+	Node* next;
+	int cost;
+};
 
-    for (auto& next : graph[node]) {
-        if (!visited[next.first]) {
-            pair<int, int> candidate = dfs(next.first, dist + next.second, visited);
-            if (candidate.first > farthest.first) {
-                farthest = candidate;
-            }
-        }
-    }
+struct Node
+{
+	int n;
+	vector<Edge> edges;
 
-    return farthest;
+	Node(int n)
+	{
+		this->n = n;
+	}
+};
+
+pair<int, Node*> Dfs(Node* startNode, vector<Node*>& graph)
+{
+	stack<Node*> dfs;
+	dfs.push(startNode);
+
+	vector<int> visited(graph.size(), -1);
+
+	visited[startNode->n] = 0;
+	int maxCost = 0;
+	Node* maxNode = 0;
+
+	while (!dfs.empty())
+	{
+		Node* node = dfs.top();
+		int cost = visited[node->n];
+		dfs.pop();
+
+		if (maxCost < cost)
+		{
+			maxCost = cost;
+			maxNode = node;
+		}
+
+		for (Edge& edge : graph[node->n]->edges)
+		{
+			if (visited[edge.next->n] == -1)
+			{
+				visited[edge.next->n] = cost + edge.cost;
+				dfs.push(edge.next);
+			}
+		}
+	}
+
+	return { maxCost, maxNode };
 }
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n; cin >> n;
-    graph.resize(n + 1);
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
 
-    for (int i = 0; i < n - 1; i++) {
-        int a, b, c; cin >> a >> b >> c;
-        graph[a].push_back({ b, c });
-        graph[b].push_back({ a, c });
-    }
+	int n; cin >> n;
+	if (n == 1)
+	{
+		cout << 0;
+		return 0;
+	}
+	vector<Node*> graph(n);
+	for (int i = 0; i < n; i++)
+	{
+		graph[i] = new Node(i);
+	}
 
-    // 1번 DFS: 임의의 노드에서 가장 먼 노드 찾기
-    vector<bool> visited(n + 1, false);
-    pair<int, int> first = dfs(1, 0, visited);
+	for (int i = 0; i < n - 1; i++)
+	{
+		int a, b, c; cin >> a >> b >> c;
+		a--, b--;
+		graph[a]->edges.push_back({ graph[b], c });
+		graph[b]->edges.push_back({ graph[a], c });
+	}
 
-    // 2번 DFS: 첫 번째 DFS에서 찾은 노드에서 가장 먼 노드 찾기
-    fill(visited.begin(), visited.end(), false);
-    pair<int, int> second = dfs(first.second, 0, visited);
-
-    // 트리의 지름 출력
-    cout << second.first << '\n';
+	auto [_, FarNode] = Dfs(graph[0], graph);
+	cout << Dfs(FarNode, graph).first;
 }
