0d857fb8f8062738eac845472a93cbcaab43ad0e
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-25 12:51:53 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -2,21 +2,25 @@
 using namespace std;
 
 int r, c;
-void DivideConquer(int n, int x, int y, long long value) {
-    if (n == 1) {
-        if (y == r && x == c) cout << value;
-        return;
-    }
+void DC(int n, int x, int y, long long value)
+{
+	if (n == 1)
+	{
+		if (y == r && x == c) cout << value;
+		return;
+	}
 
-    for (int i = 0; i < 4; i++) {
-        int xx = (i % 2) * (n / 2) + x;
-        int yy = (i / 2) * (n / 2) + y;
-        if(xx <= c && c < xx + n / 2 && yy <= r && r < yy + n / 2) DivideConquer(n / 2, xx, yy, value + (n * n / 4 * i));
-    }
+	for (int i = 0; i < 4; i++)
+	{
+		int xx = (i % 2) * (n / 2) + x;
+		int yy = (i / 2) * (n / 2) + y;
+		if (xx <= c && c < xx + n / 2 && yy <= r && r < yy + n / 2) DC(n / 2, xx, yy, value + (n * n / 4 * i));
+	}
 }
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n >> r >> c;
-    DivideConquer(pow(2, n), 0, 0, 0);
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n; cin >> n >> r >> c;
+	DC(pow(2, n), 0, 0, 0);
 }
