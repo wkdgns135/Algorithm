307f21413b0b26b54c9b56feac90b0492deae26b
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-25 12:49:43 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> v;
-bool CheckQuad(int size, int x, int y) {
-    int sum = 0;
-    for (int i = y; i < y + size; i++) {
-        for (int j = x; j < x + size; j++) {
-            sum += v[i][j] - '0';
-        }
-    }
-    if (!sum || sum == size * size)return true;
-    return false;
-}
-void DivideConquer(int size, int x, int y) {
-    if (CheckQuad(size, x, y)) {
-        cout << v[y][x];
-        return;
-    }
+void DC(vector<string>& quad, int x, int y, int size)
+{
+	int sum = 0;
+	for (int i = y; i < y + size; i++)
+	{
+		for (int j = x; j < x + size; j++)
+		{
+			sum += quad[i][j] - '0';
+		}
+	}
 
-    cout << '(';
-    for (int i = 0; i < 4; i++) {
-        int xx = x + (i % 2) * (size / 2);
-        int yy = y + (i / 2) * (size / 2);
-        DivideConquer(size / 2, xx, yy);
-    }
-    cout << ')';
+	if (sum == size * size || sum == 0)
+	{
+		cout << quad[y][x];
+		return;
+	}
+
+	cout << '(';
+	for (int i = 0; i < 4; i++)
+	{
+		int xx = x + (i % 2) * (size / 2);
+		int yy = y + (i / 2) * (size / 2);
+		DC(quad, xx, yy, size / 2);
+	}
+	cout << ')';
 }
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
-    v = vector<string>(n); for (int i = 0; i < n; i++) cin >> v[i];
-    DivideConquer(n, 0, 0);
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n; cin >> n;
+
+	vector<string> quad(n);
+	for (int i = 0; i < n; i++)cin >> quad[i];
+
+	DC(quad, 0, 0, n);
 }
