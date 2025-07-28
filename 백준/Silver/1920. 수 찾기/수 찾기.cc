5c9418402b115b10536bbb4574a3d6210239ba71
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-28 11:51:39 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,14 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    int n, m; cin >> n;
-    unordered_map<int, int> map;
-    while(n--){int input; cin >> input; map[input]++;}
-    cin >> m;
-    while(m--){
-        int input; cin >> input; 
-        if(map[input])cout << 1 << '\n';
-        else cout << 0 << '\n';
-    }
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m;
+	cin >> n;
+
+	vector<int> v1(n);
+	for (int i = 0; i < n; i++) cin >> v1[i];
+
+	cin >> m;
+	vector<int> v2(m);
+	for (int i = 0; i < m; i++) cin >> v2[i];
+
+	sort(v1.begin(), v1.end());
+
+	for (int num : v2)
+	{
+		int left = 0, right = n - 1;
+		bool result = 0;
+		if (v1[left] == num || v1[right] == num)
+		{
+			cout << 1 << '\n';
+			continue;
+		}
+		while (left <= right)
+		{
+			int mid = (left + right) / 2;
+			if (v1[mid] == num)
+			{
+				result = 1;
+				break;
+			}
+			else if (v1[mid] < num)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+		}
+		cout << result << '\n';
+	}
 }
