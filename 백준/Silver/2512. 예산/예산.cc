7a8d1f3c61ce42eed6a82b3e2173135ad992eed6
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-29 19:24:52 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.cc
+++ 백준/Silver/2512. 예산/예산.cc
@@ -1,32 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n; cin >> n;
-    vector<int> v(n); for (int i = 0; i < n; i++) cin >> v[i];
-    int m; cin >> m;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m; cin >> n;
+	vector<int> v(n);
+	int left = 0, right = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+		right = max(v[i], right);
+	}
 
-    int left = 0, right = 100000, mid, maxVal = *max_element(v.begin(), v.end());
-    int answer = 0;
+	cin >> m;
+	int answer = 0;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
 
-    while (left <= right) {
-        mid = (left + right) / 2;
-        if (maxVal < mid) {
-            right = mid - 1;
-            continue;
-        }
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += min(v[i], mid);
-        }
-        if (sum <= m) {
-            answer = mid;
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-    cout << answer;
+		long long sum = 0;
+		for (int i : v) sum += min(i, mid);
+
+		if (sum <= m)
+		{
+			answer = mid;
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	cout << answer;
 }
