a0b5744307356378d43622e827d283ee9f243722
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-31 23:05:19 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3076 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
-    vector<vector<int>> v(n);
-    vector<vector<int>> dp(n);
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n; cin >> n;
+	vector<vector<int>> v(n), dp(n);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i + 1; j++) {
-            int input; cin >> input;
-            v[i].push_back(input);
-            dp[i].push_back(0);
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		v[i].resize(i + 1);
+		dp[i].resize(i + 1);
+		for (int j = 0; j <= i; j++)
+		{
+			cin >> v[i][j];
+		}
+	}
 
-    dp[0][0] = v[0][0];
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < v[i].size(); j++) {
-            int left = j - 1 < 0 ? 0 : dp[i - 1][j - 1];
-            int right = j == v[i-1].size() ? 0 : dp[i - 1][j];
-            dp[i][j] = v[i][j] + max(left, right);
-        }
-    }
-
-    cout << *max_element(dp[n - 1].begin(), dp[n - 1].end());
+	dp[0][0] = v[0][0];
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == i) dp[i][j] = dp[i - 1][j - 1] + v[i][j];
+			else if (j == 0) dp[i][j] = dp[i - 1][j] + v[i][j];
+			else dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + v[i][j];
+		}
+	}
+	cout << *max_element(dp[n - 1].begin(), dp[n - 1].end());
 }
