e119f99276c7f042d5847cab3bf852272d6b81ed
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-07-31 23:21:30 +0900

    [Silver I] Title: 신입 사원, Time: 444 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int t; cin >> t;
-    while (t--) {
-        int n; cin >> n;
-        vector<pair<int, int>> v(n);
-        for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
-        sort(v.begin(), v.end());
-        
-        int minVal = n + 1;
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i].second < minVal) {
-                minVal = v[i].second;
-                answer++;
-            }
-        }
-        cout << answer << '\n';
-    }
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int t; cin >> t;
+	while (t--)
+	{
+		int n; cin >> n;
+		vector<pair<int, int>> v(n);
+		for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
+		sort(v.begin(), v.end());
+
+		int answer = n, minValue = v[0].second;
+		for (int i = 1; i < n; i++)
+		{
+			if (minValue > v[i].second) minValue = v[i].second;
+			else answer--;
+
+		}
+		cout << answer << '\n';
+	}
 }
