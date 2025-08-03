f1b237beae03a3a0c9f8eaae144f7d41259850e3
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-03 20:04:25 +0900

    [Gold IV] Title: 타임머신, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,40 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bf(int start, int n, int m, vector<tuple<int,int,int>> &edges) {
-    vector<long long> mdt(n + 1, 1e9);
-    mdt[start] = 0;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < m; j++) {
-            int s = get<0>(edges[j]);
-            int e = get<1>(edges[j]);
-            int t = get<2>(edges[j]);
+	int n, m; cin >> n >> m;
+	vector<vector<pair<int, int>>> graph(n);
 
-            if (mdt[s] == 1e9)continue;
-            if (mdt[e] > mdt[s] + t) {
-                mdt[e] = mdt[s] + t;
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c; cin >> a >> b >> c;
+		a--, b--;
+		graph[a].push_back({ c, b });
+	}
 
-                if (i == n) {
-                    cout << -1 << '\n';
-                    return;
-                }
-            }
-        }
-    }
+	vector<long> mdt(n, INT_MAX);
+	mdt[0] = 0;
 
-    for (int i = 2; i <= n; i++)
-        cout << (mdt[i] == 1e9 ? -1 : mdt[i]) << '\n';
-}
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int node = 0; node < n; node++)
+		{
+			if (mdt[node] == INT_MAX) continue;
+			for (auto [nextCost, nextNode] : graph[node])
+			{
+				if (mdt[nextNode] > mdt[node] + nextCost)
+				{
+					mdt[nextNode] = mdt[node] + nextCost;
+				}
+			}
+		}
+	}
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n, m, s, e, t;
-    cin >> n >> m;
-    vector<tuple<int,int,int>> edges;
-    for (int i = 0; i < m; i++) {
-        cin >> s >> e >> t;
-        edges.push_back({ s, e, t });
-    }
-    bf(1, n, m, edges);
+	for (int node = 0; node < n; node++)
+	{
+		for (auto [nextCost, nextNode] : graph[node])
+		{
+			if (mdt[node] == INT_MAX) continue;
+			if (mdt[nextNode] > mdt[node] + nextCost)
+			{
+				cout << -1 << '\n';
+				return 0;
+			}
+		}
+	}
+
+	for (int node = 1; node < n; node++)
+	{
+		cout << (mdt[node] == INT_MAX ? -1 : mdt[node]) << '\n';
+	}
 }
