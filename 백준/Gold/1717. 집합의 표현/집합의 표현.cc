bd03e2cc7faa4e86808c7d7a35531e5455b98c56
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-08 00:55:54 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> v;
-int Find(int x) {
-    if (x == v[x])return x;
-    return v[x] = Find(v[x]);
+vector<int> parent;
+
+int Find(int x)
+{
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
 }
 
-void Union(int a, int b) {
-    a = Find(a);
-    b = Find(b);
-    if (a == b)return;
+void Union(int a, int b)
+{
+	int aa = Find(a);
+	int bb = Find(b);
 
-    if (a < b)v[b] = a;
-    else v[a] = b;
+	if (aa > bb) parent[bb] = aa;
+	else parent[aa] = bb;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, m; cin >> n >> m;
-    v = vector<int>(n + 1);
-    for (int i = 1; i <= n; i++)v[i] = i;
-    while (m--) {
-        int oper, a, b; cin >> oper >> a >> b;
-        if (oper) {
-            if (Find(a) == Find(b))cout << "YES" << '\n';
-            else cout << "NO" << '\n';
-        }
-        else {
-            Union(a, b);
-        }
-    }
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n, m; cin >> n >> m;
+
+	parent.resize(n + 1);
+	for (int i = 0; i <= n; i++) parent[i] = i;
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c; cin >> c >> a >> b;
+
+		if (c) cout << (Find(a) == Find(b) ? "YES" : "NO") << '\n';
+		else Union(a, b);
+	}
 }
