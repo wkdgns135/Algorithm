48ef5917b40fcf8553efa003929401597809a24e
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-12 19:49:48 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, m; cin >> n >> m;
-    
-    vector<int> v(101, INT_MAX);
-    unordered_map<int, int> map;
-    for (int i = 0; i < n + m; i++) {
-        int a, b; cin >> a >> b;
-        map[a] = b;
-    }
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m; cin >> n >> m;
 
-    stack<int> bfs;
-    bfs.push(1);
-    v[1] = 0;
+	vector<int> graph(100, -1), visited(100, INT_MAX);
 
-    while (!bfs.empty()) {
-        int index = bfs.top();
-        int count = v[index];
-        bfs.pop();
+	for (int i = 0; i < n + m; i++)
+	{
+		int a, b; cin >> a >> b;
+		a--, b--;
+		graph[a] = b;
+	}
 
-        if (map[index] != 0) {
-            if (v[map[index]] > count) {
-                v[map[index]] = count;
-                bfs.push(map[index]);
-            }
-            continue;
-        }
+	queue<int> bfs;
+	bfs.push(0);
+	visited[0] = 0;
 
-        for (int i = 1; i <= 6; i++) {
-            int next = index + i;
-            if (next < 101 && v[next] > count + 1) {
-                v[next] = count + 1;
-                bfs.push(next);
-            }
-        }
-    }
-    cout << v[100];
+	while (!bfs.empty())
+	{
+		int node = bfs.front();
+		bfs.pop();
+
+		if (node == 99)
+		{
+			cout << visited[node];
+			break;
+		}
+		for (int i = node + 1; i <= node + 6 && i < 100; i++)
+		{
+			int next = graph[i] == -1 ? i : graph[i];
+			if (visited[next] > visited[node] + 1)
+			{
+				visited[next] = visited[node] + 1;
+				bfs.push(next);
+			}
+		}
+	}
 }
