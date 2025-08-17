90717647540c56770c3d2d757c0d7912edb36380
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-17 16:15:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,27 +1,23 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 using namespace std;
 
-struct Item {
-    int weight;
-    int value;
-};
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n, k; cin >> n >> k;
+	vector<vector<int>> dp(n + 1, vector<int>(k + 1));
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, k; cin >> n >> k;
-    vector<vector<int>> dp(n + 1, vector<int>(k+1));
-    vector<Item> v(n + 1);
+	for (int i = 1; i <= n; i++)
+	{
+		int w, v; cin >> w >> v;
 
-    for (int i = 1; i <= n; i++) {
-        int weight, value; cin >> weight >> value;
-        v[i] = { weight, value };
-    }
+		for (int j = 1; j <= k; j++)
+		{
+			if (w > j) dp[i][j] = dp[i - 1][j];
+			else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v);
+		}
+	}
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= k; j++) {
-            if (j < v[i].weight) dp[i][j] = dp[i - 1][j];
-            else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j -v[i].weight] + v[i].value);
-        }
-    }
-    cout << dp[n][k];
+	cout << dp[n][k];
 }
