17cc72b74f599ceb721ecfe789bd3dcb7b0726b2
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-17 17:41:59 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 32 ms, Memory: 5728 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,48 +1,62 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <limits.h>
 using namespace std;
 
-int n, e, v1, v2;
+#define CHECK(x) if (x == INT_MAX) { cout << -1; return 0; }
 
-int FindShortcut(vector<vector<pair<int, int>>> &v, int start, int end) {
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
-    vector<int> mdt(n, INT_MAX);
-    pq.push({ 0, start});
-    mdt[start] = 0;
+int n, e;
+vector<vector<pair<int, int>>> graph;
 
-    while (!pq.empty()) {
-        int cost = pq.top().first;
-        int node = pq.top().second;
-        pq.pop();
-        if (cost != mdt[node])continue;
-        if (node == end)break;
-        for (auto& edge : v[node]) {
-            if (mdt[edge.first] > edge.second + cost) {
-                mdt[edge.first] = edge.second + cost;
-                pq.push({ mdt[edge.first], edge.first });
-            }
-        }
-    }
+int ShortCut(int start, int end)
+{
+	vector<int> mdt(n + 1, INT_MAX);
 
-    return mdt[end];
+	priority_queue<pair<int, int>> pq;
+	pq.push({ 0, start });
+	mdt[start] = 0;
+
+	while (!pq.empty())
+	{
+		auto [cost, node] = pq.top();
+		pq.pop();
+
+		if (mdt[node] != -cost) continue;
+		if (node == end) break;
+		for (auto [nextCost, nextNode] : graph[node])
+		{
+			if (mdt[nextNode] > nextCost - cost)
+			{
+				mdt[nextNode] = nextCost - cost;
+				pq.push({ -mdt[nextNode], nextNode });
+			}
+		}
+	}
+
+	return mdt[end];
 }
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> e;
-    vector<vector<pair<int,int>>> v(n, vector<pair<int,int>>());
-    for (int i = 0; i < e; i++) {
-        int a, b, c; cin >> a >> b >> c;
-        v[a - 1].push_back({ b - 1, c });
-        v[b - 1].push_back({ a - 1, c });
-    }
-    cin >> v1 >> v2;
-    v1--, v2--;
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n >> e;
+	graph.resize(n + 1);
 
-    int av1 = FindShortcut(v, 0, v1), v1v2 = FindShortcut(v, v1, v2), v2n = FindShortcut(v, v2, n - 1);
-    int av2 = FindShortcut(v, 0, v2), v1n = FindShortcut(v, v1, n - 1);
+	while (e--)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph[a].push_back({ c, b });
+		graph[b].push_back({ c, a });
+	}
 
-    if (av1 == INT_MAX || v1v2 == INT_MAX || v2n == INT_MAX) { cout << -1; return 0; }
-    if (av2 == INT_MAX || v1n == INT_MAX) { cout << -1; return 0; }
-    
-    cout << min(av1 + v1v2 + v2n, av2 + v1v2 + v1n);
+	int v1, v2; cin >> v1 >> v2;
+	int sv1 = ShortCut(1, v1); CHECK(sv1);
+	int sv2 = ShortCut(1, v2); CHECK(sv2);
+	int v1v2 = ShortCut(v1, v2); CHECK(v1v2);
+	int v1n = ShortCut(v1, n); CHECK(v1n);
+	int v2n = ShortCut(v2, n); CHECK(v2n);
+
+	cout << min(sv1 + v1v2 + v2n, sv2 + v1v2 + v1n);
 }
