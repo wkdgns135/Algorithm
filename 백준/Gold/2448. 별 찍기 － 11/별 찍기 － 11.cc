c0ba19ab62978d88c7e872bc034178d26a10fca4
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-18 12:56:41 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 384 ms, Memory: 6980 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -1,43 +1,47 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 using namespace std;
 
-vector<vector<bool>> v;
-
-void triangle(int x, int y, bool z) {
-    v[y][x] = z;
-    v[y + 1][x - 1] = z;
-    v[y + 1][x + 1] = z;
-    for (int i = 0; i < 5; i++) {
-        v[y + 2][x - 2 + i] = z;
-    }
+void Fill(vector<vector<bool>>& v, int y, int x)
+{
+	v[y][x] = 1;
+	v[y + 1][x - 1] = 1; v[y + 1][x + 1] = 1;
+	for (int i = -2; i <= 2; i++) v[y + 2][x + i] = 1;
 }
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n; cin >> n;
-    v.resize(n, vector<bool>(n * 2));
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	int n; cin >> n;
+	int width = n * 2 - 1;
+	vector<vector<bool>> v(n, vector<bool>(width));
+	vector<vector<bool>> toggle(n, vector<bool>(width));
 
-    queue<pair<int, int>> q;
-    q.push({ n, 0 });
+	auto Toggle = [&](int y, int x)
+		{
+			if (y < n && x < width && x >= 0 && y >= 0)
+				toggle[y][x] = !toggle[y][x];
+		};
 
-    for (int depth = 0; depth < n; depth += 3) {
-        int size = q.size();
+	Fill(v, 0, n - 1);
+	Toggle(3, n - 4);
+	Toggle(3, n + 2);
 
-        for (int i = 0; i < size; i++) {
-            auto [x, y] = q.front();
-            q.pop();
-            if (y > 0 && x > 0 && v[y - 1][x + 1] && v[y - 1][x - 1])continue;
-            triangle(x, y, true);
-            q.push({ x - 3, y + 3 });
-            q.push({ x + 3, y + 3 });
-            
-        }
-    }
+	for (int i = 1; i < n / 3; i++)
+	{
+		int y = i * 3;
+		for (int x = 0; x < width; x++)
+		{
+			if (!toggle[y][x]) continue;
+			Fill(v, y, x);
+			Toggle(y + 3, x - 3);
+			Toggle(y + 3, x + 3);
+		}
+	}
 
-    for (int y = 0; y < n; y++) {
-        for (int x = 1; x < n * 2; x++) {
-            cout << (v[y][x] ? '*' : ' ');
-        }
-        cout << '\n';
-    }
+	for (const auto& row : v)
+	{
+		for (bool col : row) cout << (col ? '*' : ' ');
+		cout << '\n';
+	}
 }
