9d8dadc596cdd15831759f0e7b469fcb7cd487b7
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-22 12:15:58 +0900

    [Gold III] Title: 파티, Time: 32 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -4,7 +4,7 @@
 #include <limits.h>
 using namespace std;
 
-void short_cut(vector<int>& mdt, vector<vector<pair<int, int>>>& graph, int start)
+void short_cut(vector<int>& mdt, vector<vector<pair<int, int>>>& graph, int start, int end)
 {
 	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 	mdt[start] = 0;
@@ -15,7 +15,7 @@ void short_cut(vector<int>& mdt, vector<vector<pair<int, int>>>& graph, int star
 		pq.pop();
 
 		if (mdt[node] != cost) continue;
-
+		if (node == end) break;
 		for (auto [nextNode, nextCost] : graph[node])
 		{
 			if (mdt[nextNode] > nextCost + cost)
@@ -40,13 +40,13 @@ int main()
 	}
 	x--;
 	vector<int> x2other(n, INT_MAX);
-	short_cut(x2other, graph, x);
+	short_cut(x2other, graph, x, -1);
 	int answer = 0;
 	for (int i = 0; i < n; i++)
 	{
 		if (i == x)continue;
 		vector<int> result(n, INT_MAX);
-		short_cut(result, graph, i);
+		short_cut(result, graph, i, x);
 		answer = max(answer, result[x] + x2other[i]);
 	}
 	cout << answer;
