8513e010f070c850c7ac2ad747e3d8184d2b5ea4
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-08-30 16:37:28 +0900

    [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.cc
+++ 백준/Gold/1043. 거짓말/거짓말.cc
@@ -1,57 +1,80 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 using namespace std;
 
+vector<int> parent, truth, depth;
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n, m, f; cin >> n >> m >> f;
-    vector<bool> facts;
-    facts.resize(n + 1);
-    vector<vector<int>> parties(m);
-    vector<set<int>> graph(n + 1);
-    for (int i = 0; i < f; i++) {
-        int input; cin >> input;
-        facts[input] = true;
-    }
-    for (int i = 0; i < m; i++) {
-        int input; cin >> input;
-        parties[i].resize(input);
-        for (int j = 0; j < input; j++)cin >> parties[i][j];
-        for (int j = 0; j < input; j++) {
-            for (int k = j + 1; k < input; k++) {
-                graph[parties[i][j]].insert(parties[i][k]);
-                graph[parties[i][k]].insert(parties[i][j]);
-            }
-        }
-    }
+int Find(int x)
+{
+	if (x == parent[x]) return x;
+	return parent[x] = Find(parent[x]);
+}
 
-    for (int i = 0; i <= n; i++) {
-        if (!facts[i])continue;
-        queue<int> bfs;
-        bfs.push(i);
-        vector<bool> visited(n + 1);
-        visited[i] = true;
+void Union(int a, int b)
+{
+	int aa = Find(a);
+	int bb = Find(b);
 
-        while (!bfs.empty()) {
-            int node = bfs.front();
-            bfs.pop();
+	if (depth[aa] > depth[bb]) parent[bb] = aa;
+	else
+	{
+		parent[aa] = bb;
+		if (depth[aa] == depth[bb]) depth[bb]++;
+	}
+}
 
-            for (auto& next : graph[node]) {
-                if (visited[next])continue;
-                bfs.push(next);
-                visited[next] = true;
-                facts[next] = true;
-            }
-        }
-    }
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m; cin >> n >> m;
 
-    int answer = 0;
-    for (auto& party : parties) {
-        if (!any_of(party.begin(), party.end(), [&](int person) {
-            return facts[person];
-            })) {
-            answer++;
-        }
-    }
-    cout << answer;
+	int t; cin >> t;
+	truth.resize(n + 1);
+	parent.resize(n + 1);
+	depth.resize(n + 1);
+
+	for (int i = 0; i < t; i++)
+	{
+		int idx; cin >> idx;
+		truth[idx] = 1;
+	}
+	for (int i = 1; i <= n; i++) parent[i] = i;
+
+	vector<vector<int>> v(m);
+
+	for (int i = 0; i < m; i++)
+	{
+		int input; cin >> input;
+		v[i].resize(input);
+		for (int j = 0; j < input; j++) cin >> v[i][j];
+
+		for (int j = 1; j < input; j++)
+		{
+			Union(v[i][0], v[i][j]);
+		}
+	}
+
+	for (auto& row : v)
+	{
+		for (int idx : row)
+		{
+			if (truth[idx] || truth[parent[idx]])
+			{
+				truth[Find(idx)] = true;
+			}
+		}
+	}
+
+	for (auto& row : v)
+	{
+		for (int i : row)
+		{
+			if (truth[i] || truth[Find(i)])
+			{
+				m--; break;
+			}
+		}
+	}
+
+	cout << m;
 }
