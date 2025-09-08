b26bfa5b742fe47c6a200c85e956b4f698872d6e
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-09-08 12:52:57 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -9,13 +9,18 @@ int main()
 	cin.tie(0)->sync_with_stdio(0);
 	int c, n; cin >> c >> n;
 	vector<pair<int, int>> v(n + 1);
-	for (int i = 1; i <= n; i++) cin >> v[i].first >> v[i].second;
-	vector<int> dp(c * 100 + 1);
+	int maxVal = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> v[i].first >> v[i].second;
+		maxVal = max(maxVal, v[i].first);
+	}
+	vector<int> dp(c * maxVal + 1);
 
 	for (int i = 1; i <= n; i++)
 	{
 		auto [cost, value] = v[i];
-		for (int j = cost; j <= c * 100; j++)
+		for (int j = cost; j <= c * maxVal; j++)
 		{
 			dp[j] = max(dp[j], dp[j - cost] + value);
 		}
