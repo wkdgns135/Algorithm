2925d620b09a624fbd083a63f4c11f481465cb80
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-09-08 13:32:03 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,29 +1,20 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
 	int c, n; cin >> c >> n;
-	vector<pair<int, int>> v(n + 1);
-	int maxVal = 0;
-	for (int i = 1; i <= n; i++)
+	vector<int> dp(c + 101, INT_MAX);
+	dp[0] = 0;
+	for (int i = 0; i < n; i++)
 	{
-		cin >> v[i].first >> v[i].second;
-		maxVal = max(maxVal, v[i].first);
-	}
-	vector<int> dp(c * maxVal + 1);
-
-	for (int i = 1; i <= n; i++)
-	{
-		auto [cost, value] = v[i];
-		for (int j = cost; j <= c * maxVal; j++)
+		int cost, value; cin >> cost >> value;
+		for (int j = value; j <= c + 100; j++)
 		{
-			dp[j] = max(dp[j], dp[j - cost] + value);
+			if (dp[j - value] == INT_MAX) continue;
+			dp[j] = min(dp[j], dp[j - value] + cost);
 		}
 	}
-	cout << lower_bound(dp.begin(), dp.end(), c) - dp.begin();
+	cout << *min_element(dp.begin() + c, dp.begin() + c + 100);
 }
