e0c8c8e293dc1ee5ad1704d748a8a59dd8029f02
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2026-02-27 21:18:57 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.30 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> dp(n + 1);
+    dp[1] = 1, dp[2] = 2;
+    for(int i = 3; i <= n; i++){
+        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] %= 1000000007;
+    }
+    return dp[n];
+}
+
+// 1 2 3 5
