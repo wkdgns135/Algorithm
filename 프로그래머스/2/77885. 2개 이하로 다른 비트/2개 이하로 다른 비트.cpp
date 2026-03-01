422ac0b6dec6b3501b7e145f1762e6f083b3e1d0
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2026-03-01 20:26:20 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 31.67 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer(numbers.size());
+    
+    for(int i = 0; i < numbers.size(); i++){
+        answer[i] = numbers[i] + 1;
+        if(__builtin_popcountll(answer[i] ^ numbers[i]) > 2){
+            answer[i] = (long long)(answer[i] + ((numbers[i] ^ answer[i]) >> 2));
+        }
+    }
+    
+    return answer;
+}
