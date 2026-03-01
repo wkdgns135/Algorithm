77b36e4bb448c810389daf5dd3e62846c47a3503
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2026-03-01 20:27:30 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.82 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -10,7 +10,7 @@ vector<long long> solution(vector<long long> numbers) {
     for(int i = 0; i < numbers.size(); i++){
         answer[i] = numbers[i] + 1;
         if(__builtin_popcountll(answer[i] ^ numbers[i]) > 2){
-            answer[i] = (long long)(answer[i] + ((numbers[i] ^ answer[i]) >> 2));
+            answer[i] += (numbers[i] ^ answer[i]) >> 2;
         }
     }
     
