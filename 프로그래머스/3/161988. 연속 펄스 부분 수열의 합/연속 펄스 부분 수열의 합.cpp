72cf0fcb4d6205076f4bb1fd593eb6648ae547ea
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2026-03-25 20:28:24 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 1.78 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -5,19 +5,18 @@ using namespace std;
 
 long long solution(vector<int> sequence) {
     long long answer = 0;
-    vector<vector<long long>> dp(2, vector<long long>(500000));
     
-    dp[0][0] = sequence[0], dp[1][0] = sequence[0] * -1;
-    answer = max(answer, max(dp[0][0], dp[1][0]));
-    
-    for(int i = 1; i < sequence.size(); i++){
+    long long prev = 0, prev_opp = 0;
+    for(int i = 0; i < sequence.size(); i++){
         long long num = sequence[i];
-        long long opposite = num * -1;
         
-        dp[0][i] = max(dp[1][i - 1] + num, num);
-        dp[1][i] = max(dp[0][i - 1] + opposite, opposite);
+        long long curr = max(prev_opp + num, num);
+        long long curr_opp = max(prev - num, -num);
         
-        answer = max(answer, max(dp[0][i], dp[1][i]));
+        answer = max(answer, max(curr, curr_opp));
+        
+        prev = curr;
+        prev_opp = curr_opp;
     }
     
     return answer;
