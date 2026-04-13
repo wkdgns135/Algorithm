1d438293a532a42a022d16e86600b7f479fb6b8e
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2026-04-13 13:08:03 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.28 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <queue>
+#include <climits>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+bool isValid(int n, int x){
+    return x < n && x >= 0;
+}
+
+int solution(vector<vector<int>> board) {
+    int answer = 0;
+    int n = board.size();
+    vector<vector<vector<int>>> mdt(n, vector<vector<int>>(n, vector<int>(4, INT_MAX)));
+    queue<tuple<int,int,int>> bfs;
+    fill(mdt[0][0].begin(), mdt[0][0].end(), 0);
+    if (n > 1 && board[0][1] == 0) {
+        bfs.push({1, 0, 0});
+        mdt[0][1][0] = 100;
+    }
+    if (n > 1 && board[1][0] == 0) {
+        bfs.push({0, 1, 1});
+        mdt[1][0][1] = 100;
+    }
+    
+    static constexpr int offset[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+    
+    while(!bfs.empty()){
+        auto [x, y, d] = bfs.front();
+        bfs.pop();
+        
+        for(int i = 0; i < 4; i++){
+            int xx = x + offset[i][1];
+            int yy = y + offset[i][0];
+            int cost = d == i ? 100 : 600;
+            if(isValid(n, xx) && isValid(n, yy) && !board[yy][xx] && mdt[yy][xx][i] > mdt[y][x][d] + cost){
+                mdt[yy][xx][i] = mdt[y][x][d] + cost;
+                bfs.push({xx, yy, i});
+            }
+        }
+    }
+    
+    return *min_element(mdt[n - 1][n - 1].begin(), mdt[n - 1][n - 1].end());
+}
